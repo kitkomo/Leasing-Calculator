4a918e51fdc6c1ea1165127fb24569ba85edf26f
quive <unjusodal@gmail.com>
2023-02-14 17:34:38 +0400

    calculator logic added

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
 	customRangeInput()
+	calculator()
 })
 
 function customRangeInput() {
@@ -10,3 +11,97 @@ function customRangeInput() {
 		e.addEventListener('input', () => e.style.setProperty('--value', e.value));
 	}
 }
+
+function calculator() {
+
+	//Auto price
+	const autoPriceRange = document.querySelector('#auto-price-range')
+	const autoPriceNum = document.querySelector('#auto-price-num')
+
+	let autoPrice = autoPriceNum.value
+
+	autoPriceNum.addEventListener('input', () => {
+		assignValuesNum()
+	})
+
+	autoPriceRange.addEventListener('input', () => {
+		assignValuesRange()
+	})
+
+	//Initioal payment
+	const initialPaymentRange = document.querySelector('#initial-payment-range')
+	const initialPaymentPercent = document.querySelector('#initial-payment-percent')
+	const initialPaymentNum = document.querySelector('#initial-payment-num')
+
+	initialPaymentRange.addEventListener('input', () => {
+		assignValuesRange()
+	})
+
+	initialPaymentNum.addEventListener('input', () => {
+		assignValuesNum()
+	})
+
+	//Period
+	const periodRange = document.querySelector('#period-range')
+	const periodNum = document.querySelector('#period-num')
+
+	let period = periodNum.value
+
+	periodNum.addEventListener('input', () => {
+		assignValuesNum()
+	})
+
+	periodRange.addEventListener('input', () => {
+		assignValuesRange()
+	})
+
+	//Assigment funcs
+
+	function assignValuesNum() {
+		autoPrice = autoPriceNum.value
+		autoPriceNum.value = autoPrice
+		autoPriceRange.value = autoPrice
+
+		///
+
+		period = periodNum.value
+		periodNum.value = period
+		periodRange.value = period
+
+		///
+
+		initialPaymentPercent.textContent = Math.floor((initialPaymentNum.value * 100) / autoPriceNum.value)
+		initialPaymentRange.value = initialPaymentPercent.value
+
+		totalValues()
+	}
+
+	function assignValuesRange() {
+		autoPrice = autoPriceRange.value
+		autoPriceNum.value = autoPrice
+		autoPriceRange.value = autoPrice
+
+		///
+
+		period = periodRange.value
+		periodNum.value = period
+		periodRange.value = period
+
+		///
+
+		initialPaymentPercent.textContent = initialPaymentRange.value + '%'
+		initialPaymentNum.value = Math.floor((autoPriceNum.value * initialPaymentRange.value) / 100)
+
+		totalValues()
+	}
+
+	const totalValues = () => {
+		const total = document.querySelector('#total')
+		const payment = document.querySelector('#monthly-payment')
+
+		payment.textContent = Math.floor((autoPriceNum.value - initialPaymentNum.value) * (0.05 * Math.pow((1 + 0.05), periodNum.value) / (Math.pow((1 + 0.05), periodNum.value) - 1)))
+
+		total.textContent = Math.floor((periodNum.value * Number(payment.textContent) + Number(initialPaymentNum.value)))
+	}
+}
+
