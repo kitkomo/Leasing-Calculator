68e581dee478ffe80dbf8c5d63fe2c57accb2564
quive <unjusodal@gmail.com>
2023-02-15 11:43:59 +0400

    code refactoring

--- src/js/index.js
+++ src/js/index.js
@@ -1,166 +1,169 @@
 document.addEventListener('DOMContentLoaded', () => {
-	validator()
-	customRangeInput()
-	calculator()
+  validator()
+  customRangeInput()
+  calculator()
 })
 
-function customRangeInput() {
-	for (let e of document.querySelectorAll('input[type="range"].slider-progress')) {
-		e.style.setProperty('--value', e.value);
-		e.style.setProperty('--min', e.min == '' ? '0' : e.min);
-		e.style.setProperty('--max', e.max == '' ? '100' : e.max);
-		e.addEventListener('input', () => e.style.setProperty('--value', e.value));
-	}
-	
-	const auto = document.querySelector('#auto-price-num')
-	const period = document.querySelector('#period-num')
-	const inputs = [auto, period]
+function validator() {
+  const inputs = document.querySelectorAll('.form__input')
 
-	inputs.forEach(item => {
-		item.addEventListener('change', () => {
-			item.parentElement.lastElementChild.style.setProperty('--value', item.value)
-		})
-	})
+  inputs.forEach((item) => {
+    item.addEventListener('input', (e) => {
+      item.value = e.target.value.replace(/\D/g, '')
+    })
+  })
 }
 
-function validator() {
-	const inputs = document.querySelectorAll('.form__input')
+function customRangeInput() {
+  for (let e of document.querySelectorAll('input[type="range"].slider-progress')) {
+    e.style.setProperty('--value', e.value)
+    e.style.setProperty('--min', e.min == '' ? '0' : e.min)
+    e.style.setProperty('--max', e.max == '' ? '100' : e.max)
+    e.addEventListener('input', () => e.style.setProperty('--value', e.value))
+  }
 
-	inputs.forEach(item => {
-		item.addEventListener('input', (e) => {
-			item.value = e.target.value.replace(/\D/g, '')
-		})
-	})
+  const auto = document.querySelector('#auto-price-num')
+  const period = document.querySelector('#period-num')
+  const inputs = [auto, period]
+
+  inputs.forEach((item) => {
+    item.addEventListener('change', () => {
+      item.parentElement.lastElementChild.style.setProperty('--value', item.value)
+    })
+  })
 }
 
 function calculator() {
-	//Auto price
-	const autoPriceRange = document.querySelector('#auto-price-range')
-	const autoPriceNum = document.querySelector('#auto-price-num')
+  const autoPriceRange = document.querySelector('#auto-price-range')
+  const autoPriceNum = document.querySelector('#auto-price-num')
 
-	let autoPrice = autoPriceNum.value
+  const initialPaymentRange = document.querySelector('#initial-payment-range')
+  const initialPaymentPercent = document.querySelector('#initial-payment-percent')
+  const initialPaymentNum = document.querySelector('#initial-payment-num')
 
-	autoPriceNum.addEventListener('change', () => {
-		if (autoPriceNum.value >= 1500000 && autoPriceNum.value <= 10000000) {
-			assignValuesNum()
-		} else {
-			let difference = 10000000 - autoPriceNum.value
-			if (difference <= ((10000000 - 1500000) / 2)) {
-				autoPriceNum.value = 10000000
-				assignValuesNum()
-			} else {
-				autoPriceNum.value = 1500000
-				assignValuesNum()
-			}
-		}
-		
-	})
+  const periodRange = document.querySelector('#period-range')
+  const periodNum = document.querySelector('#period-num')
 
-	autoPriceRange.addEventListener('input', () => {
-		assignValuesRange()
-	})
+  const total = document.querySelector('#total')
+  const payment = document.querySelector('#monthly-payment')
 
-	//Initioal payment
-	const initialPaymentRange = document.querySelector('#initial-payment-range')
-	const initialPaymentPercent = document.querySelector('#initial-payment-percent')
-	const initialPaymentNum = document.querySelector('#initial-payment-num')
+  const mask = new Intl.NumberFormat('ru', {
+    style: 'currency',
+    currency: 'rub',
+    minimumFractionDigits: 0,
+  })
 
-	initialPaymentNum.addEventListener('change', () => {
-		const precalculatedPercents = Math.floor((initialPaymentNum.value * 100) / autoPriceNum.value)
-		if (precalculatedPercents <= 60 && precalculatedPercents >= 10) {
-			assignValuesNum()
-		} else {
-			let difference = precalculatedPercents - 60
-			if (difference >= (precalculatedPercents - 60) / 2) {
-				initialPaymentNum.value = Math.floor((autoPriceNum.value * 60) / 100)
-				assignValuesNum()
-			} else {
-				initialPaymentNum.value = Math.floor((autoPriceNum.value * 10) / 100)
-				assignValuesNum()
-			}
-		}
-	})
+  function calcMontlyPayment() {
+    return Math.floor(
+      (autoPriceNum.value - initialPaymentNum.value) * ((0.05 * Math.pow(1 + 0.05, periodNum.value)) / (Math.pow(1 + 0.05, periodNum.value) - 1)),
+    )
+  }
 
-	initialPaymentRange.addEventListener('input', () => {
-		assignValuesRange()
-	})
+  function calcTotalPayment() {
+    return Math.floor(periodNum.value * Number(payment.textContent) + Number(initialPaymentNum.value))
+  }
 
-	//Period
-	const periodRange = document.querySelector('#period-range')
-	const periodNum = document.querySelector('#period-num')
-	let period = periodNum.value
+  function calcPaymentPercent(percents = 100) {
+    return Math.floor((initialPaymentNum.value * percents) / autoPriceNum.value)
+  }
 
-	periodNum.addEventListener('change', () => {
-		if (periodNum.value <= 120 && periodNum.value >= 6) {
-			assignValuesNum()
-		} else {
-			let difference = 120 - periodNum.value
-			if (difference <= 0) {
-				periodNum.value = 120
-				assignValuesNum()
-			} else {
-				periodNum.value = 6
-				assignValuesNum()
-			}
-		}
-	})
+  function calcInitialPayment(percents) {
+    return Math.floor((autoPriceNum.value * percents) / 100)
+  }
 
-	periodRange.addEventListener('input', () => {
-		assignValuesRange()
-	})
+  //memory variables
+  let autoPrice = autoPriceNum.value
+  let period = periodNum.value
 
-	//Assigment funcs
-	function assignValuesNum() {
-		autoPrice = autoPriceNum.value
-		autoPriceNum.value = autoPrice
-		autoPriceRange.value = autoPrice
-		///
-		period = periodNum.value
-		periodNum.value = period
-		periodRange.value = period
-		///
-		if (Math.floor((initialPaymentNum.value * 100) / autoPriceNum.value) < 10 || Math.floor((initialPaymentNum.value * 100) / autoPriceNum.value) > 60) {
-			initialPaymentNum.value = Math.floor((autoPriceNum.value * 13) / 100)
-			initialPaymentPercent.textContent = Math.floor((initialPaymentNum.value * 100) / autoPriceNum.value) + '%'
-		} else {
-			initialPaymentPercent.textContent = Math.floor((initialPaymentNum.value * 100) / autoPriceNum.value) + '%'
-		}
-		initialPaymentRange.value = Number((initialPaymentPercent.textContent).slice(0, -1))
-		initialPaymentRange.style.setProperty('--value', initialPaymentRange.value)
-		totalValues()
-	}
+  //listening auto
+  autoPriceNum.addEventListener('change', () => {
+    valueValid(1500000, 10000000, autoPriceNum)
+    assignValuesNum()
+  })
+  autoPriceRange.addEventListener('input', () => {
+    assignValuesRange()
+  })
 
-	function assignValuesRange() {
-		autoPrice = autoPriceRange.value
-		autoPriceNum.value = autoPrice
-		autoPriceRange.value = autoPrice
-		///
-		period = periodRange.value
-		periodNum.value = period
-		periodRange.value = period
-		///
-		initialPaymentPercent.textContent = initialPaymentRange.value + '%'
-		initialPaymentNum.value = Math.floor((autoPriceNum.value * initialPaymentRange.value) / 100)
-	
-		totalValues()
-	}
+  //listening period
+  periodNum.addEventListener('change', () => {
+    valueValid(6, 120, periodNum)
+    assignValuesNum()
+  })
+  periodRange.addEventListener('input', () => {
+    assignValuesRange()
+  })
 
-	const totalValues = () => {
-		const total = document.querySelector('#total')
-		const payment = document.querySelector('#monthly-payment')
+  //listening initial payment
+  initialPaymentNum.addEventListener('change', () => {
+    const preCalcPercents = calcPaymentPercent(100)
+    if (preCalcPercents < 10 || preCalcPercents > 60) {
+      let difference = preCalcPercents - 60
+      if (difference >= (preCalcPercents - 60) / 2) {
+        initialPaymentNum.value = calcInitialPayment(60)
+      } else {
+        initialPaymentNum.value = calcInitialPayment(10)
+      }
+    }
+    assignValuesNum()
+  })
+  initialPaymentRange.addEventListener('input', () => {
+    assignValuesRange()
+  })
 
-		payment.textContent = Math.floor((autoPriceNum.value - initialPaymentNum.value) * (0.05 * Math.pow((1 + 0.05), periodNum.value) / (Math.pow((1 + 0.05), periodNum.value) - 1)))
+  //Assigment funcs
+  function assignValuesNum() {
+    autoPrice = autoPriceNum.value
+    autoPriceNum.value = autoPrice
+    autoPriceRange.value = autoPrice
+    ///
+    period = periodNum.value
+    periodNum.value = period
+    periodRange.value = period
+    ///
+    if (calcPaymentPercent() < 10 || calcPaymentPercent() > 60) {
+      initialPaymentNum.value = calcInitialPayment(13)
+      initialPaymentPercent.textContent = calcPaymentPercent() + '%'
+    } else {
+      initialPaymentPercent.textContent = calcPaymentPercent() + '%'
+    }
 
-		total.textContent = Math.floor((periodNum.value * Number(payment.textContent) + Number(initialPaymentNum.value)))
+    initialPaymentRange.value = Number(initialPaymentPercent.textContent.slice(0, -1))
+    initialPaymentRange.style.setProperty('--value', initialPaymentRange.value) //for range slider to work
 
-		const mask = new Intl.NumberFormat('ru', {
-			style: 'currency',
-			currency: 'rub',
-			minimumFractionDigits: 0
-		})
+    totalValues()
+  }
 
-		payment.textContent = mask.format(Number(payment.textContent))
-		total.textContent = mask.format(Number(total.textContent))
-	}
+  function assignValuesRange() {
+    autoPrice = autoPriceRange.value
+    autoPriceNum.value = autoPrice
+    autoPriceRange.value = autoPrice
+    ///
+    period = periodRange.value
+    periodNum.value = period
+    periodRange.value = period
+    ///
+    initialPaymentPercent.textContent = initialPaymentRange.value + '%'
+    initialPaymentNum.value = Math.floor((autoPriceNum.value * initialPaymentRange.value) / 100)
+
+    totalValues()
+  }
+
+  const totalValues = () => {
+    payment.textContent = calcMontlyPayment()
+    total.textContent = calcTotalPayment()
+
+    payment.textContent = mask.format(Number(payment.textContent))
+    total.textContent = mask.format(Number(total.textContent))
+  }
+
+  function valueValid(min, max, input) {
+    if (input.value < min || input.value > max) {
+      const difference = max - input.value
+      if (difference <= (max - min) / 2) {
+        input.value = max
+      } else {
+        input.value = min
+      }
+    }
+  }
 }
-
